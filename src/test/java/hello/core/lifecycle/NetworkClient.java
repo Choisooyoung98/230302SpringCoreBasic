31e64455cb92ccd6a72c898babc2578b65cc2648
Choisooyoung98 <ekfms15815@gmail.com>
2023-03-19 03:09:30 +0900

    어노테이션 @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -28,14 +31,23 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
     }
+
+    /*
+    @PostConstruct, @PreDestroy 어노테이션을 사용하자
+    최신 스프링에서 가장 권장하는 방법.
+    외부 라이브러리에는 적용하지 못하는 것이 단점 Bean으로 등록해서 사용해야됨.
+    코드를 고칠 수 없는 라이브러리를 초기화, 종료해야 하면 Bean의 initMethod, destroyMethod를 사용하자.
+    * */
 }
